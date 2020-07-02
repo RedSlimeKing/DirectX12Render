9f1cba7a7657b2a4ca1f449a92c24ee4a1109b15
RedSlimeKing <DevonALy@outlook.com>
2020-07-01 22:01:12 -0400

    Added shader code and check fxc

--- DirectXproject/DirectXproject/DirectXAPI.cpp
+++ DirectXproject/DirectXproject/DirectXAPI.cpp
@@ -133,14 +133,14 @@ bool DirectXAPI::CheckTearingSupport(){
 	// graphics debugging tools which will not support the 1.5 factory interface 
 	// until a future update.
 	ComPtr<IDXGIFactory4> factory4;
-	/*if(SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory4)))){
+	if(SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory4)))){
 		ComPtr<IDXGIFactory5> factory5;
 		if(SUCCEEDED(factory4.As(&factory5))){
 			if(FAILED(factory5->CheckFeatureSupport( DXGI_FEATURE_PRESENT_ALLOW_TEARING, &allowTearing, sizeof(allowTearing)))){
 				allowTearing = FALSE;
 			}
 		}
-	}*/
+	}
 
 	return (allowTearing == TRUE);
 }
--- DirectXproject/DirectXproject/VertexShader.hlsl
+++ DirectXproject/DirectXproject/VertexShader.hlsl
@@ -1,4 +1,32 @@
 
-void main( uint3 DTid : SV_DispatchThreadID )
+struct VertexPosColor
 {
+	float3 Position : POSITION;
+	float3 Color    : COLOR;
+};
+
+struct VertexShaderOutput
+{
+	float4 Color    : COLOR;
+	float4 Position : SV_Position;
+};
+
+struct ModelViewProjection
+{
+	matrix MVP;
+};
+
+cbuffer ModelViewProjectionCB : register(b0)
+{
+	ModelViewProjection ModelViewProjectionCB;
 }
+
+VertexShaderOutput main(VertexPosColor IN)
+{
+	VertexShaderOutput OUT;
+
+	OUT.Position = mul(ModelViewProjectionCB.MVP, float4(IN.Position, 1.0f));
+	OUT.Color = float4(IN.Color, 1.0f);
+
+	return OUT;
+};
